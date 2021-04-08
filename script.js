0db2beff219ba80a0f4b5abbcbe161836659d168
hassen <>
2021-04-08 12:48:53 +0000

    almost finish

--- script.js
+++ script.js
@@ -17,37 +17,6 @@ for (let i = 0; i < names.length; i++) {
 
 
 
-var timer = new Timer();
-$('#chronoExample .startButton').click(function () {
-    timer.start();
-});
-
-$('#chronoExample .pauseButton').click(function () {
-    timer.pause();
-});
-
-$('#chronoExample .stopButton').click(function () {
-    timer.stop();
-});
-
-$('#chronoExample .resetButton').click(function () {
-    timer.reset();
-});
-
-timer.addEventListener('secondsUpdated', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
-
-timer.addEventListener('started', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
-
-timer.addEventListener('reset', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
-
-
-
 
 
 
